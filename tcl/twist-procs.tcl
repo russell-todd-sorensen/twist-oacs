fc85682771a1b0e596495f74b6e747519de060d2
Russell <russell.todd.sorensen@gmail.com>
2016-11-20 16:40:54 -0800

    Update to version 0.9.31
    Added new git download and unpacking procedures

--- tcl/twist-procs.tcl
+++ tcl/twist-procs.tcl
@@ -1,52 +1,106 @@
 namespace eval ::twist {
+    
 }
 
 namespace eval ::twist::install {
 
     variable installed 0
-    variable version "0.9.30"
+    variable version "0.9.31"
     variable package "twist"
     variable sourceDirectory [ns_info tcllib] 
     variable packageDirectory [file dirname [file dirname [info script]]]
-    variable versionDirectory [file join $sourceDirectory $package $package-$version]
+    variable versionDirectory [file join $packageDirectory $package-$version]
     variable linkWebservices 0
     variable linkDocumentation 0
+    variable releaseType "tar.gz"
+    variable repositoryUrl "https://github.com/russell-todd-sorensen"
+    variable errorFile error-$package-$version.txt
+    variable downloadFile ""
 }
- 
+
 proc ::twist::install::init { } {
     
     # Ensure variables are up-to-date with database:
-    variable package twist
+    variable package
+    
     variable installed [parameter::get_from_package_key -package_key $package -parameter installed -default 0]
     variable upgradeNow [parameter::get_from_package_key -package_key $package -parameter upgradeNow -default 0]
-    variable version [parameter::get_from_package_key -package_key $package -parameter version -default 0.9.10]
+    variable version [parameter::get_from_package_key -package_key $package -parameter version -default 0.9.31]
     variable sourceDirectory [parameter::get_from_package_key -package_key $package -parameter sourceDirectory -default ""]
+    
     if {"$sourceDirectory" eq ""} {
         set sourceDirectory [ns_info tcllib]
         if {$installed} {
             parameter::set_from_package_key -package_key $package -parameter sourceDirectory -value $sourceDirectory
         }
     }
+    
     variable versionDirectory [file join $sourceDirectory $package $package-$version]
-    variable linkWebservices [parameter::get_from_package_key -package_key $package -parameter linkWebservices -default 0]
+    variable linkWebservices [parameter::get_from_package_key -package_key $package -parameter linkWebservices -default 1]
     variable linkDocumentation [parameter::get_from_package_key -package_key $package -parameter linkDocumentation -default 1]
+    
     return $installed
 }
 
-
-proc ::twist::install::before-install { } {
+proc ::twist::install::downloadFile { } {
 
     variable version
     variable package
     variable versionDirectory
+    variable packageDirectory
+    variable releaseType
+    variable repositoryUrl
+    variable errorFile
+    variable downloadFile
+
+    # Checkout:
+    set downloadFile $packageDirectory/$package-${version}.$releaseType
+    
+    exec wget $repositoryUrl/$package/archive/${version}.${releaseType} -O $downloadFile 2> [file join $packageDirectory $errorFile]
+  
+}
+
+proc ::twist::install::deleteFile { } {
+    variable downloadFile
+    file delete $downloadFile
+}
+
+proc ::twist::install::unpackFile { } {
+
+    variable releaseType
+    variable downloadFile
+    variable packageDirectory
+    variable errorFile
+    variable owner
+    variable group
+
+    array set tclLibAttributesArray [file attributes [ns_info tcllib]] 
+    set owner $tclLibAttributesArray(-owner)
+    set group $tclLibAttributesArray(-group)
+    
+    switch -exact -- $releaseType {
+	"tar.gz" {
+	    exec tar xf $downloadFile\
+		--directory $packageDirectory\
+		--owner $owner\
+		--group $group\
+		2>> [file join $packageDirectory $errorFile]
+	}
+	"zip" {
+
+	}
+    }
+
+}
+
+proc ::twist::install::before-install { } {
 
     # initialize
     init
 
-    # Checkout:
-    # exec http:///$package-$version $versionDirectory
-    # provide a replacement for the above command 
-    
+    # download new version
+    downloadFile
+    unpackFile
 }
 
 proc ::twist::install::deleteFileLink { linkName } {
